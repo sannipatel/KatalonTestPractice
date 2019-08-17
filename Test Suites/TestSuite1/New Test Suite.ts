e79582b4f49e6c0eec49802b11fbc82eee89336e
new <new@DESKTOP-M0VA8UU>
2019-08-17 19:16:53 +0530

    new commit

---
+++ Test Suites/TestSuite1/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9a62e054-bb28-4186-a374-03e0cc3ae9e6</testSuiteGuid>
+   <testCaseLink>
+      <guid>063b3301-6bad-4810-a1e1-132cca79091c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample Test CAse</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
